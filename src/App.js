747dbc6bd74ccba0dcfb6e0615a3407ed4d50fd9
Spacemin8 <userspace118@gmail.com>
2024-05-08 07:34:10 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header/Header';
-import SimpleBottomNavigation from './components/MainNav';
-import Movies from './Pages/Movies/Movies';
-import Series from './Pages/Series/Series';
-import Trending from './Pages/Trending/Trending';
-import Search from './Pages/Search/Search';
-import { Container } from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@material-ui/core";
 
 function App() {
   return (
